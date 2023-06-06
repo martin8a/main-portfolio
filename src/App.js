c091adec86d2d52f084ef4b58994ee4046cd2f3e
martin8a <64031382+martin8a@users.noreply.github.com>
2023-06-05 23:31:07 -0500

    First commit portfolio

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.scss';
+import './resources/fonts.scss';
+import { Route, Routes } from 'react-router-dom';
+
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element></Route>
+      </Routes>
+    </>
+  );
+}
+
+export default App;
